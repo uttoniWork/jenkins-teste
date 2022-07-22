71f64303420865e1993f367bcb714508ecc93f41
uttoni <ubrandani@uolinc.com>
2022-07-22 16:57:03 -0300

    other-branch falando oi

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,11 @@
 public class Application {
 
     public static void main(String[] args) {
+
         System.out.println("Main class");
+
+        final Speaker speaker = new Speaker();
+
+        System.out.println(speaker.sayHi());
     }
 }
